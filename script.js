ec5b0fe3bdb20599ef74b8fe437be1305254fadc
Vitaly Kniazev <vitaly.kniazev@gmail.com>
2020-11-18 17:53:42 +0300

    fix: optimize onScroll() handler

--- script.js
+++ script.js
@@ -3,18 +3,12 @@ document.addEventListener('scroll' ,onScroll);
 function onScroll(event) {
     const curPos = window.scrollY;
     const blocks = document.querySelectorAll('#home, #services, #portfolio, #about, #contact');
-    const links = document.querySelectorAll('a.navmenu-item-link');
-    console.log(blocks);
-    console.log(links);
-
+    const headerHeight = document.querySelector('.header').offsetHeight;
     blocks.forEach((element) => {
-        if (element.offsetTop <= (curPos + 96) && (element.offsetTop +  element.offsetHeight) > curPos) {
-            links.forEach((a) => {
-                a.classList.remove('navmenu-item-link-selected');
-                if (element.getAttribute('id') === a.getAttribute('href').substring(1)) {
-                    a.classList.add('navmenu-item-link-selected');
-                }
-            })
-        }
-    });
+        if ((element.offsetTop <= curPos + headerHeight) && (element.offsetTop +  element.offsetHeight) > curPos) {
+            document.querySelector('.navmenu-item-link-selected').classList.remove('navmenu-item-link-selected');
+            document.querySelector(`a[href="#${element.getAttribute('id')}"]`).classList.add('navmenu-item-link-selected');
+            }
+        });
+    
 }
