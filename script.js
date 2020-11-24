fcf5f77110557cf38f1249177133d3a057b005f6
Vitaly Kniazev <vitaly.kniazev@gmail.com>
2020-11-24 16:09:34 +0300

    fix: enable more smooth changing of slides

--- script.js
+++ script.js
@@ -44,59 +44,52 @@ const sliderParams = {
   sliderContentListClass: ".slides-list",
   slideClass: ".slide",
   sliderArrowClass: ".slider-arrow",
+  sliderBackgroundClass: ".slider-background",
 };
 const slides = document.querySelectorAll(sliderParams.slideClass);
 const arrows = document.querySelectorAll(sliderParams.sliderArrowClass);
 const slider = document.querySelector(sliderParams.sliderClass);
 const sliderContentList = document.querySelector(sliderParams.sliderContentListClass);
 const sliderContent = document.querySelector(sliderParams.sliderContentClass);
+const sliderBackgrounds = document.querySelectorAll(sliderParams.sliderBackgroundClass + " > *");
 
 var sliderWidth = sliderContent.offsetWidth;
 arrows.forEach((arrow) => arrow.addEventListener("click", moveSlide));
 
 var [currentSlide, nextSlide] = [0, 1];
 
+var direction;
+var isNewBackground = false;
+
 slides.forEach((element, index) => {
   element.style.width = sliderWidth + "px";
+  element.addEventListener("transitionend", onSlideChanged);
 });
+
+function onSlideChanged() {
+  if (isNewBackground) {
+    sliderBackgrounds[(-1 * direction + 1) / 2].style.backgroundColor = window.getComputedStyle(slides[currentSlide]).backgroundColor;
+    isNewBackground = false;
+  }
+}
 
 slides[currentSlide].style.left = "0px";
 slides[nextSlide].style.left = sliderWidth + "px";
 
-/*
 function moveSlide(event) {
   const arrow = event.target;
-  const direction = +arrow.getAttribute("data-direction");
-  sliderWidth = sliderContent.offsetWidth;
-  if (direction > 0) {
-    slides[nextSlide].style.left = `-${sliderWidth}px`;
-  } else {
-    slides[nextSlide].style.left = `${sliderWidth}px`;
-  }
-  slides[currentSlide].style.left = direction * sliderWidth + "px";
-  slides[nextSlide].style.left = "0px";
-  [currentSlide, nextSlide] = [nextSlide, currentSlide];
-  slider.style.backgroundColor = window.getComputedStyle(slides[currentSlide]).backgroundColor;
-}
-*/
-
-function moveSlide(event) {
-  const arrow = event.target;
-  const direction = +arrow.getAttribute("data-direction");
+  direction = +arrow.getAttribute("data-direction");
   let next = sliderContentList.removeChild(slides[nextSlide]);
   sliderWidth = sliderContent.offsetWidth;
-  if (direction > 0) {
-    next.style.left = `-${sliderWidth}px`;
-  } else {
-    next.style.left = `${sliderWidth}px`;
-  }
-  slides[currentSlide].style.left = direction * sliderWidth + "px";
+  next.style.left = direction * sliderWidth + "px";
+  slides[currentSlide].style.left = -1 * direction * sliderWidth + "px";
+  isNewBackground = true;
   setTimeout(() => {
     next.style.left = "0px";
   });
   sliderContentList.appendChild(next);
+  sliderBackgrounds[(direction + 1) / 2].style.backgroundColor = window.getComputedStyle(slides[nextSlide]).backgroundColor;
   [currentSlide, nextSlide] = [nextSlide, currentSlide];
-  slider.style.backgroundColor = window.getComputedStyle(slides[currentSlide]).backgroundColor;
 }
 
 function onResizeWindow() {
