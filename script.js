cfd5af559a5dfada50b13357aed3c50597060b37
Vitaly Kniazev <vitaly.kniazev@gmail.com>
2020-12-05 01:14:46 +0300

    feat: add script to play the samples

---
+++ script.js
@@ -0,0 +1,22 @@
+const appParams = {
+  classDrumSelector: ".drum",
+  classKeypressed: "keypressed",
+};
+
+const drums = document.querySelectorAll(appParams.classDrumSelector);
+drums.forEach((key) => key.addEventListener("animationend", removeAnimationStyle));
+window.addEventListener("keydown", playSample);
+
+function playSample(event) {
+  const sample = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const drum = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  if (!sample) return;
+  drum.classList.add(appParams.classKeypressed);
+  sample.currentTime = 0;
+  sample.play();
+}
+
+function removeAnimationStyle(event) {
+  event.target.classList.remove(appParams.classKeypressed);
+  console.log(event);
+}
