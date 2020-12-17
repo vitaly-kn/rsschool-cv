6e44a1efe66f5d41ea7b78585b4ad449e61831fd
Vitaly Kniazev <vitaly.kniazev@gmail.com>
2020-12-17 22:11:18 +0300

    refact: improve js code

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const appParams = {
   classSpeedBar: ".speed-bar",
 };
 
+const MIN_SPEED = 0.4;
+const MAX_SPEED = 4;
+
 const video = document.querySelector(appParams.classVideoContent);
 const speed = document.querySelector(appParams.classSpeed);
 const hint = document.querySelector(appParams.classHint);
@@ -15,10 +18,8 @@ speed.addEventListener("mousemove", changePlaybackRate);
 function changePlaybackRate(event) {
   const y = event.currentTarget.offsetTop + event.currentTarget.offsetHeight - event.pageY;
   const percent = y / event.currentTarget.offsetHeight;
-  const min = 0.4;
-  const max = 4;
   const height = Math.round(percent * 100) + "%";
-  const playbackRate = (percent * (max - min) + min).toFixed(1);
+  const playbackRate = (percent * (MAX_SPEED - MIN_SPEED) + MIN_SPEED).toFixed(1);
   bar.style.height = height;
   hint.textContent = `${playbackRate}x`;
   video.playbackRate = playbackRate;
