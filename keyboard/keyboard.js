6a7f5cf501f59322af46f44e4320f47beb1debf1
Vitaly Kniazev <vitaly.kniazev@gmail.com>
2020-12-21 22:28:28 +0300

    fix: remove bug with mixed input from virtual and hard keyboard

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -5,15 +5,17 @@ const Keyboard = {
     keys: [],
   },
 
-  eventHandlers: {
+  /*eventHandlers: {
     oninput: null,
     onclose: null,
-  },
+  },*/
+  onInputHandler: null,
 
-  properties: {
+  /*properties: {
     value: "",
     capsLock: false,
-  },
+  },*/
+  isCapsLockPressed: false,
 
   init() {
     // Create main elements
@@ -35,8 +37,16 @@ const Keyboard = {
     // Automatically use keyboard for elements with .use-keyboard-input
     document.querySelectorAll(".use-keyboard-input").forEach((element) => {
       element.addEventListener("focus", () => {
-        this.open(element.value, function (currentValue) {
+        //console.log(`${element} focused!`);
+        /*this.open(element.value, function (currentValue) {
           element.value = currentValue;
+        });*/
+        this.open(function (character) {
+          if (character !== "\b") {
+            element.value += character;
+          } else {
+            element.value = element.value.substring(0, element.value.length - 1);
+          }
         });
       });
     });
@@ -72,8 +82,11 @@ const Keyboard = {
           /*eyElement.innerHTML = createIconHTML("backspace");*/
 
           keyElement.addEventListener("click", () => {
-            this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
-            this._triggerEvent("oninput");
+            //this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+            //this._triggerEvent("oninput");
+            //this.properties.value += "\b";
+            //this.onInputHandler(this.properties.value);
+            this.onInputHandler("\b");
           });
 
           break;
@@ -84,7 +97,7 @@ const Keyboard = {
 
           keyElement.addEventListener("click", () => {
             this._toggleCapsLock();
-            keyElement.classList.toggle("keyboard__key_active", this.properties.capsLock);
+            keyElement.classList.toggle("keyboard__key_active", this.isCapsLockPressed);
           });
 
           break;
@@ -94,8 +107,10 @@ const Keyboard = {
           /*keyElement.innerHTML = createIconHTML("keyboard_return");*/
 
           keyElement.addEventListener("click", () => {
-            this.properties.value += "\n";
-            this._triggerEvent("oninput");
+            //this.properties.value += "\n";
+            //this._triggerEvent("oninput");
+            //this.onInputHandler(this.properties.value);
+            this.onInputHandler("\n");
           });
 
           break;
@@ -105,8 +120,10 @@ const Keyboard = {
           /*keyElement.innerHTML = createIconHTML("space_bar");*/
 
           keyElement.addEventListener("click", () => {
-            this.properties.value += " ";
-            this._triggerEvent("oninput");
+            //this.properties.value += " ";
+            //this._triggerEvent("oninput");
+            //this.onInputHandler(this.properties.value);
+            this.onInputHandler(" ");
           });
 
           break;
@@ -118,7 +135,7 @@ const Keyboard = {
 
           keyElement.addEventListener("click", () => {
             this.close();
-            this._triggerEvent("onclose");
+            //this._triggerEvent("onclose");
           });
 
           break;
@@ -127,8 +144,11 @@ const Keyboard = {
           //keyElement.textContent = key.toLowerCase();
 
           keyElement.addEventListener("click", () => {
-            this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
-            this._triggerEvent("oninput");
+            //this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+            //const character = this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+            const character = this.isCapsLockPressed ? key.toUpperCase() : key.toLowerCase();
+            //this._triggerEvent("oninput");
+            this.onInputHandler(character);
           });
 
           break;
@@ -143,36 +163,40 @@ const Keyboard = {
 
     return fragment;
   },
-
-  _triggerEvent(handlerName) {
+  /*_onInputHandler(element)*/
+  /*_triggerEvent(handlerName) {
     //if (typeof this.eventHandlers[handlerName] == "function") {
     //Is this check really needed?
     this.eventHandlers[handlerName](this.properties.value);
     //}
-  },
+  },*/
 
   _toggleCapsLock() {
-    this.properties.capsLock = !this.properties.capsLock;
+    //this.properties.capsLock = !this.properties.capsLock;
+    this.isCapsLockPressed = !this.isCapsLockPressed;
 
     for (const key of this.elements.keys) {
       //if (key.childElementCount === 0) {
       if (key.textContent.length === 1) {
-        key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+        //key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+        key.textContent = this.isCapsLockPressed ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
       }
     }
   },
 
-  open(initialValue, oninput, onclose) {
-    this.properties.value = initialValue || "";
+  //open(initialValue, oninput, onclose) {
+  open(handler) {
+    /*this.properties.value = initialValue || "";
     this.eventHandlers.oninput = oninput;
-    this.eventHandlers.onclose = onclose;
+    this.eventHandlers.onclose = onclose;*/
+    this.onInputHandler = handler;
     this.elements.main.classList.remove("keyboard_hidden");
   },
 
   close() {
-    this.properties.value = "";
+    /*this.properties.value = "";
     this.eventHandlers.oninput = oninput;
-    this.eventHandlers.onclose = onclose;
+    this.eventHandlers.onclose = onclose;*/
     this.elements.main.classList.add("keyboard_hidden");
   },
 };
