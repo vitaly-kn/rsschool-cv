d3c46be6813db1b2ab1546cbe206737787dde225
Vitaly Kniazev <vitaly.kniazev@gmail.com>
2021-01-06 01:12:18 +0300

    init: add Raindrops project

---
+++ src/script.js
@@ -0,0 +1,22 @@
+import "./style.css";
+
+const appParams = {
+  classHighligthed: "highlighted",
+  classShake: "shake",
+  classUp: "up",
+  classHoleSelector: ".hole",
+  classMoleSelector: ".mole",
+  idTheme: "theme",
+  idHit: "hit",
+  idUp: "up",
+  idScore: "score",
+  idHiScore: "hi-score",
+  idLevel: "level",
+  idTime: "time",
+  idBonusTime: "bonus-time",
+  idStart: "start",
+  animationDelay: 500,
+  timerStep: 100,
+  playTime: 26000,
+  baseMoleDelay: 2000,
+};
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+//const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./script.js",
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 4200,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+    /*new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/assets/sound/"),
+          to: path.resolve(__dirname, "dist/assets/sound/"),
+        },
+      ],
+    }),*/
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/img",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/fonts",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(mp3|wav)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/sound",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
